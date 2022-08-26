37d1f7b03f6fc940bf010c66bf2adbd99f665a70
edy mejia <serviciosedy@gmail.com>
2022-08-26 12:07:36 -0300

    te

--- l10n_do_accounting/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from . import res_partner
-from . import res_company
-from . import l10n_latam_document_type
-from . import account_journal
-from . import account_move
-from . import monkey_patch
-from . import account_move_line
