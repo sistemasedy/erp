5d003efea65bb73f5a67eb2346dd7a3e9640bf8f
edy mejia <serviciosedy@gmail.com>
2022-09-12 09:04:48 -0300

    te

--- bsi_pos_clear_cart/static/src/js/pos_clear_cart.js
+++ bsi_pos_clear_cart/static/src/js/pos_clear_cart.js
@@ -34,7 +34,7 @@ odoo.define('bsi_pos_clear_cart.ClearCart', function(require) {
                 while(order.get_selected_orderline()) {
                     order.remove_orderline(order.get_selected_orderline())
                 }
-                console.log(clients)  
+                console.log(this.clients)  
             }
             
        }
