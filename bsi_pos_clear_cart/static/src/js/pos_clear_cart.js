6e99f003c98e66e60ccd2418a03ab16a1ef52fde
edy mejia <serviciosedy@gmail.com>
2022-09-12 20:47:02 -0400

    te

--- bsi_pos_clear_cart/static/src/js/pos_clear_cart.js
+++ bsi_pos_clear_cart/static/src/js/pos_clear_cart.js
@@ -6,6 +6,8 @@ odoo.define('bsi_pos_clear_cart.ClearCart', function(require) {
        const Registries = require('point_of_sale.Registries');
        const models = require("point_of_sale.models");
 
+       models.load_fields("res.partner", ["credit"]);
+
 
 
       
