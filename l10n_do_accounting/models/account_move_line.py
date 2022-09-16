8f16fad0498f5bb49e904c9276d49cd60fac3246
edy mejia <serviciosedy@gmail.com>
2022-09-16 11:35:46 -0300

    te

--- l10n_do_accounting/models/account_move_line.py
+++ l10n_do_accounting/models/account_move_line.py
@@ -8,7 +8,6 @@ class AccountMoveLine(models.Model):
         string="ITBIS Amount",
         store=True,
         readonly=True,
-        currency_field="always_set_currency_id",
     )
 
     def _get_price_total_and_subtotal(
