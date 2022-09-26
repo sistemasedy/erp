66d26bc293a72e17bdde5f87193c312ad182c8b7
edy mejia <serviciosedy@gmail.com>
2022-09-26 08:56:13 -0300

    te

--- em_pos_credit/static/src/js/pos_client.js
+++ em_pos_credit/static/src/js/pos_client.js
@@ -6,7 +6,7 @@ odoo.define('em_pos_credit.PosClient', function(require) {
        const Registries = require('point_of_sale.Registries');
        const models = require("point_of_sale.models");
 
-       models.load_fields("res.partner", ["credit", "credit_limit"]);
+       models.load_fields("res.partner", ["credit", "credit_limit", "due_amount"]);
        //models.load_fields("res.partner", ["credit", "credit_limit", "due_amount", "l10n_do_dgii_tax_payer_type"]);
 
 
