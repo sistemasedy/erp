2f5306a1e70ff7b1dd802b564a2cb4436f9aad11
edy mejia <serviciosedy@gmail.com>
2022-09-29 16:11:08 -0300

    te

--- em_pos_credit/static/src/js/pos_client.js
+++ em_pos_credit/static/src/js/pos_client.js
@@ -37,7 +37,7 @@ odoo.define('em_pos_credit.PosClient', function(require) {
 	        async validateOrder(isForceValidate) {
 	        	const order = this.currentOrder;
 	        	const currentClient = this.currentOrder.get_client();
-	        	console.log("test", order.name)
+	        	console.log("test", order.paymentlines.name)
 	            if (currentClient.due_amount > currentClient.credit_limit) {
 	            	this.showPopup('ErrorPopup', {
 		                title: this.env._t('Customer Account Control'),
