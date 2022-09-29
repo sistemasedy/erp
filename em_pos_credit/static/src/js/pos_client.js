378f68dec0096a267e240618b6ab8cff38e7f56d
edy mejia <serviciosedy@gmail.com>
2022-09-29 15:36:02 -0300

    te

--- em_pos_credit/static/src/js/pos_client.js
+++ em_pos_credit/static/src/js/pos_client.js
@@ -37,21 +37,30 @@ odoo.define('em_pos_credit.PosClient', function(require) {
 	        async validateOrder(isForceValidate) {
 	        	const currentClient = this.currentOrder.get_client();
 	        	console.log("test", currentClient)
-	            if(this.env.pos.config.cash_rounding) {
-	                if(!this.env.pos.get_order().check_paymentlines_rounding()) {
-	                    this.showPopup('ErrorPopup', {
-	                        title: this.env._t('Rounding error in payment lines'),
-	                        body: this.env._t("The amount of your payment lines must be rounded to validate the transaction."),
-	                    });
-	                    return;
-	                }
-	            }
-	            if (await this._isOrderValid(isForceValidate)) {
-	                // remove pending payments before finalizing the validation
-	                for (let line of this.paymentLines) {
-	                    if (!line.is_done()) this.currentOrder.remove_paymentline(line);
-	                }
-	                await this._finalizeValidation();
+	            if (currentClient.due_amount > currentClient.credit_limit) {
+	            	this.showPopup('ErrorPopup', {
+		                title: this.env._t('Rounding error in payment lines'),
+		                body: this.env._t("The amount of your payment lines must be rounded to validate the transaction."),
+		            });
+		            return;
+
+	            }else(){
+	            	if(this.env.pos.config.cash_rounding) {
+		                if(!this.env.pos.get_order().check_paymentlines_rounding()) {
+		                    this.showPopup('ErrorPopup', {
+		                        title: this.env._t('Rounding error in payment lines'),
+		                        body: this.env._t("The amount of your payment lines must be rounded to validate the transaction."),
+		                    });
+		                    return;
+		                }
+		            }
+		            if (await this._isOrderValid(isForceValidate)) {
+		                // remove pending payments before finalizing the validation
+		                for (let line of this.paymentLines) {
+		                    if (!line.is_done()) this.currentOrder.remove_paymentline(line);
+		                }
+		                await this._finalizeValidation();
+		            }
 	            }
 	        }
 
