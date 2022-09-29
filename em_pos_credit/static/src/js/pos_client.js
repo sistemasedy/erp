8990a84cc58ef68f5330c4356be940b2c414e17c
edy mejia <serviciosedy@gmail.com>
2022-09-29 14:58:17 -0300

    te

--- em_pos_credit/static/src/js/pos_client.js
+++ em_pos_credit/static/src/js/pos_client.js
@@ -53,6 +53,28 @@ odoo.define('em_pos_credit.PosClient', function(require) {
 	        }
 
 
+	        _updateSelectedPaymentline() {
+	            if (this.paymentLines.every((line) => line.paid)) {
+	                this.currentOrder.add_paymentline(this.payment_methods_from_config[0]);
+	            }
+	            if (!this.selectedPaymentLine) return; // do nothing if no selected payment line
+	            // disable changing amount on paymentlines with running or done payments on a payment terminal
+	            const payment_terminal = this.selectedPaymentLine.payment_method.payment_terminal;
+	            if (
+	                payment_terminal &&
+	                !['pending', 'retry'].includes(this.selectedPaymentLine.get_payment_status())
+	            ) {
+	                return;
+	            }
+	            if (NumberBuffer.get() === null) {
+	                this.deletePaymentLine({ detail: { cid: this.selectedPaymentLine.cid } });
+	            } else {
+	                this.selectedPaymentLine.set_amount(NumberBuffer.getFloat());
+	            }
+	            console.log("test", currentClient)
+	        }
+
+
 
          
         }
