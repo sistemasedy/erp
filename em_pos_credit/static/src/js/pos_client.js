0b6cd162392c0d4f4b2e36c7caf2ea9ddf667132
edy mejia <serviciosedy@gmail.com>
2022-10-02 10:19:26 -0300

    te

--- em_pos_credit/static/src/js/pos_client.js
+++ em_pos_credit/static/src/js/pos_client.js
@@ -38,52 +38,77 @@ odoo.define('em_pos_credit.PosClient', function(require) {
 	        			method = order.paymentlines.models[i].amount
 	        		}
 	        	}
-	            if (method > 0 && currentClient.active_limit) {
+	            if (currentClient.active_limit) {
 
-	            	if (currentClient.due_amount + method > currentClient.blocking_stage) {
-		            	this.showPopup('ErrorPopup', {
-				            title: this.env._t('Control de Cuenta de Clientes'),
-				            body: this.env._t("El Monto Excede el limite de Credito."),
-				        });
-				        return;
+	            	if (method > 0) {
+
+		            	if (currentClient.due_amount + method > currentClient.blocking_stage) {
+			            	this.showPopup('ErrorPopup', {
+					            title: this.env._t('Control de Cuenta de Clientes'),
+					            body: this.env._t("El Monto Excede el limite de Credito."),
+					        });
+					        return;
+			            }else{
+			            	if(this.env.pos.config.cash_rounding) {
+				                if(!this.env.pos.get_order().check_paymentlines_rounding()) {
+				                    this.showPopup('ErrorPopup', {
+				                    	title: this.env._t('Rounding error in payment lines'),
+				                        body: this.env._t("The amount of your payment lines must be rounded to validate the transaction."),
+				                    });
+				                    return;
+				                }
+				            }
+				            if (await this._isOrderValid(isForceValidate)) {
+				                // remove pending payments before finalizing the validation
+				                for (let line of this.paymentLines) {
+				                    if (!line.is_done()) this.currentOrder.remove_paymentline(line);
+				                }
+				                await this._finalizeValidation();
+				            }
+			            }
+
+		            	
+
 		            }else{
-		            	if(this.env.pos.config.cash_rounding) {
-			                if(!this.env.pos.get_order().check_paymentlines_rounding()) {
-			                    this.showPopup('ErrorPopup', {
-			                    	title: this.env._t('Rounding error in payment lines'),
-			                        body: this.env._t("The amount of your payment lines must be rounded to validate the transaction."),
-			                    });
-			                    return;
-			                }
-			            }
-			            if (await this._isOrderValid(isForceValidate)) {
-			                // remove pending payments before finalizing the validation
-			                for (let line of this.paymentLines) {
-			                    if (!line.is_done()) this.currentOrder.remove_paymentline(line);
-			                }
-			                await this._finalizeValidation();
-			            }
+			            	if(this.env.pos.config.cash_rounding) {
+				                if(!this.env.pos.get_order().check_paymentlines_rounding()) {
+				                    this.showPopup('ErrorPopup', {
+				                    	title: this.env._t('Rounding error in payment lines'),
+				                        body: this.env._t("The amount of your payment lines must be rounded to validate the transaction."),
+				                    });
+				                    return;
+				                }
+				            }
+				            if (await this._isOrderValid(isForceValidate)) {
+				                // remove pending payments before finalizing the validation
+				                for (let line of this.paymentLines) {
+				                    if (!line.is_done()) this.currentOrder.remove_paymentline(line);
+				                }
+				                await this._finalizeValidation();
+				            }
 		            }
 
-	            	
-
 	            }else{
-		            	if(this.env.pos.config.cash_rounding) {
-			                if(!this.env.pos.get_order().check_paymentlines_rounding()) {
-			                    this.showPopup('ErrorPopup', {
-			                    	title: this.env._t('Rounding error in payment lines'),
-			                        body: this.env._t("The amount of your payment lines must be rounded to validate the transaction."),
-			                    });
-			                    return;
-			                }
-			            }
-			            if (await this._isOrderValid(isForceValidate)) {
-			                // remove pending payments before finalizing the validation
-			                for (let line of this.paymentLines) {
-			                    if (!line.is_done()) this.currentOrder.remove_paymentline(line);
-			                }
-			                await this._finalizeValidation();
-			            }
+
+
+	            	        if(this.env.pos.config.cash_rounding) {
+				                if(!this.env.pos.get_order().check_paymentlines_rounding()) {
+				                    this.showPopup('ErrorPopup', {
+				                    	title: this.env._t('Rounding error in payment lines'),
+				                        body: this.env._t("The amount of your payment lines must be rounded to validate the transaction."),
+				                    });
+				                    return;
+				                }
+				            }
+				            if (await this._isOrderValid(isForceValidate)) {
+				                // remove pending payments before finalizing the validation
+				                for (let line of this.paymentLines) {
+				                    if (!line.is_done()) this.currentOrder.remove_paymentline(line);
+				                }
+				                await this._finalizeValidation();
+				            }
+
+	            	
 	            }
 	        }
 
