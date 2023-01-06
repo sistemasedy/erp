9e527e0adf9a7bde226f8031060ee7cb7c0cfc6a
Almacen Gomas a Consignacion <ALMGOMASAC>
2023-01-06 14:54:26 -0400

    app tr

--- theme_common/__manifest__.py
+++ theme_common/__manifest__.py
@@ -11,6 +11,6 @@
         'views/old_snippets/s_page_header.xml',
         'views/old_snippets/s_three_columns_circle.xml',
     ],
-    'application': False,
+    'application': True,
     'license': 'LGPL-3',
 }
