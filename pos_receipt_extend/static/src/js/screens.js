cd625cc60662f65fe12aaf37be1f2c0d3def7fd4
Almacen Gomas a Consignacion <ALMGOMASAC>
2023-12-14 14:23:51 -0400

    te

---
+++ pos_receipt_extend/static/src/js/screens.js
@@ -0,0 +1,66 @@
+odoo.define('pos_l10n_ar_identification.screens', function(require) {
+    'use strict';
+
+    const ClientListScreen = require('point_of_sale.ClientListScreen');
+    const Registries = require('point_of_sale.Registries');
+    var { Gui } = require('point_of_sale.Gui');
+    var models = require('point_of_sale.models');
+
+
+
+
+    
+
+    const POSSaveClientOverride = ClientListScreen =>
+        class extends ClientListScreen {
+            constructor() {
+                super(...arguments);
+                
+                useListener('click-fiscal', () => this.env.bus.trigger('ncfTxt'));
+            }
+            /**
+             * @override
+             */
+
+         
+
+            async saveChanges(event) {
+
+
+
+                try {
+
+
+                    
+                let partnerId = await this.rpc({
+                    model: 'res.partner',
+                    method: 'create_from_ui',
+                    args: [event.detail.processedChanges],
+                });
+                await this.env.pos.load_new_partners();
+                this.state.selectedClient = this.env.pos.db.get_partner_by_id(partnerId);
+                this.state.detailIsShown = false;
+                this.render();
+            } catch (error) {
+                if (error.message.code < 0) {
+                    await this.showPopup('OfflineErrorPopup', {
+                        title: this.env._t('Offline'),
+                        body: this.env._t('Unable to save changes.'),
+                    });
+                } else {
+                    throw error;
+                }
+            }
+        
+
+
+            }
+
+
+               
+        };
+
+    Registries.Component.extend(ClientListScreen, POSSaveClientOverride);
+
+    return ClientListScreen;
+});
