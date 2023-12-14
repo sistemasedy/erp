81743b7282b48fb0f38cf99b85a1896ff085809c
Almacen Gomas a Consignacion <ALMGOMASAC>
2023-12-14 14:54:42 -0400

    te

--- pos_receipt_extend/static/src/js/screens.js
+++ pos_receipt_extend/static/src/js/screens.js
@@ -6,15 +6,10 @@ odoo.define('pos_l10n_ar_identification.screens', function(require) {
     var { Gui } = require('point_of_sale.Gui');
     var models = require('point_of_sale.models');
 
-    const Ajax = require('web.ajax');
-
-    const data_json = [];
-
-
+    
 
     const POSSaveClientOverride = ClientListScreen =>
         class extends ClientListScreen {
-            
             /**
              * @override
              */
@@ -24,35 +19,76 @@ odoo.define('pos_l10n_ar_identification.screens', function(require) {
             async saveChanges(event) {
 
 
+
                 try {
 
 
-                        
-                    let partnerId = await this.rpc({
-                        model: 'res.partner',
-                        method: 'create_from_ui',
-                        args: [event.detail.processedChanges],
+                var jm_l10n_ar_afip_responsibility_type_id=$('.l10n_ar_afip_responsibility_type_id').val();
+                var jm_l10n_latam_identification_type_id=$('.l10n_latam_identification_type_id').val();
+                var jm_identification_number=$('.identification_number').val();
+
+             
+                if (jm_l10n_ar_afip_responsibility_type_id == '0' ) {
+
+
+                         await this.showPopup('ErrorPopup', {
+                        title: this.env._t('Resp. AFIP debe completarse'),
+                        body: this.env._t('Debe completar el campo Resp. AFIP'),
                     });
-                    await this.env.pos.load_new_partners();
-                    this.state.selectedClient = this.env.pos.db.get_partner_by_id(partnerId);
-                    this.state.detailIsShown = false;
-                    this.render();
-                } catch (error) {
-                    if (error.message.code < 0) {
-                        await this.showPopup('OfflineErrorPopup', {
-                            title: this.env._t('Offline'),
-                            body: this.env._t('Unable to save changes.'),
-                        });
-                    } else {
-                        throw error;
+                         return;
+                    }else if (jm_l10n_latam_identification_type_id == '0' ) {
+
+
+                         await this.showPopup('ErrorPopup', {
+                        title: this.env._t('Tipo Doc debe completarse'),
+                        body: this.env._t('Debe completar el campo Tipo Doc'),
+                    });
+                         return;
+                    }else if (jm_identification_number == '' ) {
+
+
+                         await this.showPopup('ErrorPopup', {
+                        title: this.env._t('Numero debe completarse'),
+                        body: this.env._t('Debe completarse el campo Número de Identificación'),
+                    });
+                         return;
                     }
-                }       
+
+
+                    if (!jm_identification_number &&  jm_l10n_ar_afip_responsibility_type_id=='1' ) {
+
+
+                         await this.showPopup('ErrorPopup', {
+                        title: this.env._t('El campo CUIT (NIF) es requerido.'),
+                        body: this.env._t('El Campo CUIT es requerido'),
+                    });
+                         return;
+                    }
+
+                    
+                let partnerId = await this.rpc({
+                    model: 'res.partner',
+                    method: 'create_from_ui',
+                    args: [event.detail.processedChanges],
+                });
+                await this.env.pos.load_new_partners();
+                this.state.selectedClient = this.env.pos.db.get_partner_by_id(partnerId);
+                this.state.detailIsShown = false;
+                this.render();
+            } catch (error) {
+                if (error.message.code < 0) {
+                    await this.showPopup('OfflineErrorPopup', {
+                        title: this.env._t('Offline'),
+                        body: this.env._t('Unable to save changes.'),
+                    });
+                } else {
+                    throw error;
+                }
+            }
+        
 
 
             }
-
-
-            
 
 
                
