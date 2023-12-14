99fb452d722fec9e65be48f63765bdb93356760b
Almacen Gomas a Consignacion <ALMGOMASAC>
2023-12-14 15:39:57 -0400

    te

--- pos_receipt_extend/static/src/js/screens.js
+++ pos_receipt_extend/static/src/js/screens.js
@@ -5,6 +5,11 @@ odoo.define('pos_l10n_ar_identification.screens', function(require) {
     const Registries = require('point_of_sale.Registries');
     var { Gui } = require('point_of_sale.Gui');
     var models = require('point_of_sale.models');
+    const { useListener } = require('web.custom_hooks');
+    
+    const Ajax = require('web.ajax');
+
+    const data_json = [];
 
     
 
@@ -13,6 +18,13 @@ odoo.define('pos_l10n_ar_identification.screens', function(require) {
             /**
              * @override
              */
+
+             constructor() {
+                super(...arguments);
+                useListener('click-fiscal', this.ncfTxt);
+
+
+            }
 
          
 
@@ -40,7 +52,86 @@ odoo.define('pos_l10n_ar_identification.screens', function(require) {
                     }
                 }
 
-            }   
+            }
+
+
+            async ncfTxt() {
+
+              var list = []
+              var num_ncf = $($(document).find("#num_ncf"))[0].value;
+              if (data_json.length > 0) {
+                for (var i = 0; i < data_json.length; i++) {
+                  if (data_json[i].state == 'ACTIVO') {
+                    if (data_json[i].ncf == num_ncf) {
+                      list.push(data_json[i])
+
+                    }
+                  }
+                }
+                
+                if (list.length > 0) {
+                  var inputName = $($(document).find("input.detail.client-name"))[0].value = list[0].name;
+                  var inputVat = $($(document).find("input.detail.vat"))[0].value = list[0].ncf;
+                  $($(document).find("input.detail.client-name"))[0].focus()
+
+                  
+                }else{
+                  alert("Numero de Comprovante no Existe")
+                }
+                
+              }else{
+                var XMLReq = new XMLHttpRequest();
+                XMLReq.open( "GET", "https://raw.githubusercontent.com/sistemasedy/api_ncf/main/DGII_RNC/TMP/DGII_RNC.TXT", false )
+
+                XMLReq.onreadystatechange = function() {
+                  if(XMLReq.readyState == 4 && XMLReq.status == 200) {
+
+                    var lines = XMLReq.responseText.split("\n");
+                    //var data_json = [];
+                    var tmp;
+                    for(var index in lines){
+                      tmp = lines[index].trim().split("|");
+                      data_json.push({
+                        ncf : tmp[0],
+                        name : tmp[1],
+                        state : tmp[9]
+                      });
+                    }
+                    for (var i = 0; i < data_json.length; i++) {
+                      if (data_json[i].state == 'ACTIVO') {
+                        if (data_json[i].ncf == num_ncf) {
+                          list.push(data_json[i])
+
+                        }
+                      }
+                    }
+                    
+                    if (list.length > 0) {
+                      var inputName = $($(document).find("input.detail.client-name"))[0].value = list[0].name;
+                      var inputVat = $($(document).find("input.detail.vat"))[0].value = list[0].ncf;
+                      $($(document).find("input.detail.client-name"))[0].focus()
+                      
+                    }else{
+                      alert("Numero de Comprovante no Existe")
+                    }                   
+
+                    
+                  }
+                }
+
+                XMLReq.send();
+
+              }
+              if (true) {
+                //var inputSearch = $($(document).find("div.searchbox-client.top-content-center"))[0].value = list[0].name;
+                console.log($($(document).find("div.searchbox-client.top-content-center")))
+                console.log($($(document).find("input.detail.vat"))[0].value)
+                  
+              }     
+            }
+
+
+
         };
 
     Registries.Component.extend(ClientListScreen, POSSaveClientOverride);
