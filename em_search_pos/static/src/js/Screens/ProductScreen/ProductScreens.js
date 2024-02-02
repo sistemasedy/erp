29878601549c66fc152a9bb14ca255b506404cc5
Almacen Gomas a Consignacion <ALMGOMASAC>
2024-02-02 16:15:24 -0400

    te

--- em_search_pos/static/src/js/Screens/ProductScreen/ProductScreens.js
+++ em_search_pos/static/src/js/Screens/ProductScreen/ProductScreens.js
@@ -27,6 +27,43 @@ odoo.define('em_search_pos.ProductScreens', function (require) {
             }
 
 
+            async _getProductByBarcode(code) {
+                console.log("barra")
+                let product = this.env.pos.db.get_product_by_barcode(code.base_code);
+                if (!product) {
+                    // find the barcode in the backend
+                    let foundProductIds = [];
+                    try {
+                        foundProductIds = await this.rpc({
+                            model: 'product.product',
+                            method: 'search',
+                            args: [[
+                                ['barcode', '=', code.base_code],
+                                ['sale_ok', '=', true],
+                                ['available_in_pos', '=', true]
+                            ]],
+                            context: this.env.session.user_context,
+                        });
+                    } catch (error) {
+                        if (isConnectionError(error)) {
+                            return this.showPopup('OfflineErrorPopup', {
+                                title: this.env._t('Network Error'),
+                                body: this.env._t("Product is not loaded. Tried loading the product from the server but there is a network error."),
+                            });
+                        } else {
+                            throw error;
+                        }
+                    }
+                    if (foundProductIds.length) {
+                        await this.env.pos._addProducts(foundProductIds, false);
+                        // assume that the result is unique.
+                        product = this.env.pos.db.get_product_by_id(foundProductIds[0]);
+                    }
+                }
+                return product
+            }
+
+
             
 
            
