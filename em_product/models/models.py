b12b4b41e056e62e8f6f6cb403c9dcf5886a0f3a
Almacen Gomas a Consignacion <ALMGOMASAC>
2024-02-09 07:40:47 -0400

    te

--- em_product/models/models.py
+++ em_product/models/models.py
@@ -20,6 +20,7 @@ class ResPartner(models.Model):
     _inherit = 'res.partner'
 
     vat = fields.Char(string='RNC ', index=True, help="The Tax Identification Number. Complete it if the contact is subjected to government taxes. Used in some legal statements.")
+    otros_dias = fields.Char(string='Otros Dias', index=True)
 
 
 
