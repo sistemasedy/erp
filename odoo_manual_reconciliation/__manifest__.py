a6b6266668ddd4caf88f191a5193a8f829912b47
Almacen Gomas a Consignacion <ALMGOMASAC>
2024-02-14 14:53:41 -0400

    atualizacion

---
+++ odoo_manual_reconciliation/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Manual Reconciliation Odoo',
+    'version': '1.0',
+    'summary': 'Odoo Reconciliation, Odoo manual reconciliation, manual reconciliation, reconcilliation, quick books, quick book, quickbook, quickbooks, bank reconcile, reconcile, accounts, accounting, Reconciliation, Bank Reconciliation, Invoice Reconciliation, Payment Reconciliation, Bank Statement, Accounting Excel Reports, Odoo Excel Reports, Odoo Accounting Excel Reports, Odoo Financial Reports, Accounting Reports In Excel For Odoo 17, Financial Reports in Excel, Odoo Account Reports, inventory, banking, forecasting, cash, cashflow, cash flow, credit acount, bank, book, books, ledger, journal, journal entry, statement, statements, bank statements, transact, transaction, transactions, odoo, techfinna, ',
+    'author': 'TechFinna',
+    'website': 'https://www.techfinna.com',
+    'maintainer': 'Techfinna',
+    'category': 'Productivity',
+    'support': "info@techfinna.com",
+    'license': 'LGPL-3',
+    'price': 0,
+    'currency': 'USD',
+    'depends': ['base'],
+    'images': ['static/description/banner.gif'],
+   
+   
+    'application': True,
+    'installable': True,
+}
