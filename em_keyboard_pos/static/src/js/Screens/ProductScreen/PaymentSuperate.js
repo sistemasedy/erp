fc51af137b93841e59bba4e27348e409f2aa72fa
Almacen Gomas a Consignacion <ALMGOMASAC>
2024-02-20 16:09:30 -0400

    te

--- em_keyboard_pos/static/src/js/Screens/ProductScreen/PaymentSuperate.js
+++ em_keyboard_pos/static/src/js/Screens/ProductScreen/PaymentSuperate.js
@@ -1,4 +1,4 @@
-odoo.define('pos_margin.PaymentDirect', function(require) {
+odoo.define('pos_margin.PaymentSuperate', function(require) {
     'use strict';
 
     const PosComponent = require('point_of_sale.PosComponent');
