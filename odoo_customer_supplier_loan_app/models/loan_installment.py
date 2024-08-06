06f9a7db4916a0742b157370db34e95e4e76dcc5
Almacen Gomas a Consignacion <ALMGOMASAC>
2024-08-06 15:39:24 -0400

    te

--- odoo_customer_supplier_loan_app/models/loan_installment.py
+++ odoo_customer_supplier_loan_app/models/loan_installment.py
@@ -10,6 +10,26 @@ class LoanInstallment(models.Model):
     _description = "Loan Installment"
 
     name = fields.Char(string="Numero",readonly="True",compute="_compute_name")
+
+
+
+    partner_id = fields.Many2one('res.partner',string="Empresa",default=lambda self : self.env.user.company_id.id,required=True)
+    applied_date = fields.Date(string="Fecha",default=fields.Date.today())
+    company_id = fields.Many2one('res.company' ,default=lambda self : self.env.user.company_id.id,string="Company",required=True)
+    user_id = fields.Many2one('res.users',default=lambda self : self.env.user.id,string="User",readonly=True)
+    principal_amount = fields.Float(string="Entrada",required=True,digits=(32, 2))
+    salida = fields.Float(string="Salida",required=True,digits=(32, 2))
+    balance_on_loans = fields.Float(string="Balance On Loan", store=True)
+    notes = fields.Text(string="Notes")
+    start_date = fields.Date(string='Fecha de Inicio', default=lambda self: fields.Date.today() - timedelta(days=30))
+    end_date = fields.Date(string='Fecha de Fin', default=fields.Date.today)
+    currency_id = fields.Many2one('res.currency', 'Currency', required=True, default=lambda self: self.env.company.currency_id.id)
+    #order_line = fields.One2many('report.sale.line', 'order_id', string='Order Lines', states={'cancel': [('readonly', True)], 'done': [('readonly', True)]}, copy=True)
+    state = fields.Selection([('draft','Draft'),('applied','Applied'),('approve','Approved'),('closed','Cerrado'),('cancel','Cancel'),('disbursed','Disbursed')],default='draft')
+
+
+
+
     installment_number = fields.Integer(string="Renglon")
     date_from = fields.Date(string="Date From")
     date_to  = fields.Date(string="Date To")
