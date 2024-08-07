47b5c16f6b22fe875a17307f3972898da45b8249
Almacen Gomas a Consignacion <ALMGOMASAC>
2024-08-07 15:16:57 -0400

    te

--- l10n_do_accounting/__manifest__.py
+++ l10n_do_accounting/__manifest__.py
@@ -10,7 +10,7 @@
     "website": "https://github.com/odoo-dominicana",
     "version": "15.0.0.5.6",
     # any module necessary for this one to work correctly
-    "depends": ["l10n_latam_invoice_document", "l10n_do"],
+    "depends": ["l10n_latam_invoice_document", "l10n_do","em_product"],
     # always loaded
     "data": [
         "security/ir.model.access.csv",
