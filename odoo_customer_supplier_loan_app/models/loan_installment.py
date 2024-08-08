882d4a4ed48aac85d258826fc21fc2cf0f0799ec
Almacen Gomas a Consignacion <ALMGOMASAC>
2024-08-08 12:25:37 -0400

    te

--- odoo_customer_supplier_loan_app/models/loan_installment.py
+++ odoo_customer_supplier_loan_app/models/loan_installment.py
@@ -56,7 +56,55 @@ class LoanInstallment(models.Model):
     
 
     def action_payment(self):
-        self.write({'state':'approve'})
+        self._create_or_update_purchase_orders()
+
+    def _create_or_update_purchase_orders(self):
+        products = self.env['product.template'].search([])
+
+        if not products:
+            raise ValidationError(_("No hay Productos configurados."))
+
+        
+        
+        for product in products:
+            reorder_qty = self._get_reorder_quantity(product)
+            if reorder_qty > 0:
+                self._create_or_update_purchase_order_line(product, reorder_qty)
+                products_updated += 1
+
+        return self._reload_client_with_message(f'{products_updated} ordenes creadas o actualizadas correctamente.')
+
+ 
+
+    def _get_reorder_quantity(self, product):
+        stock_moves = self.env['stock.move'].search([
+            ('product_id', '=', product.id),
+            ('location_id.usage', '=', 'internal'),
+            ('date', '>=', self.start_date),
+            ('date', '<=', self.end_date),
+            ('reference', 'ilike', 'WH/POS/%')
+        ])
+        return abs(sum(stock_moves.mapped('product_qty')))
+
+    def _create_or_update_purchase_order_line(self, product, reorder_qty):
+        purchase_order_line_obj = self.env['report.sale.line']
+
+        purchase_order_line = purchase_order_line_obj.search([
+            ('order_id', '=', self.id),
+            ('product_id', '=', product.id)
+        ], limit=1)
+
+        if purchase_order_line:
+            purchase_order_line.product_qty = reorder_qty
+        else:
+            purchase_order_line_obj.create({
+                'order_id': self.id,
+                'product_id': product.id,
+                'name': product.name,
+                'product_qty': reorder_qty,
+                'product_uom': product.uom_po_id.id,
+                'date_planned': fields.Date.today(),
+            })
         
 
 
