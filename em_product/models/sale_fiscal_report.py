9cc316cfe1758be1afb07dc7f02f46fea2d76798
edy mejia <masterserverodo@gmail.com>
2025-02-23 11:34:05 +0000

    te

--- em_product/models/sale_fiscal_report.py
+++ em_product/models/sale_fiscal_report.py
@@ -6,12 +6,4 @@ class PurchaseFiscalReport(models.Model):
     _name = 'purchase.fiscal.report'
     _inherit = 'purchase.order' # purchase.order, NOT sale.order
 
-    
-                                 currency_field='currency_id',
-                                 readonly=True,
-                                 store=True,
-                                 compute='_compute_ncf_total',
-                                 help="Total de la venta para el NCF (con impuesto).")
-    ncf_day = fields.Char(string='Día NCF', help="Día de la transacción para el NCF.")
-    
-        self._compute_ncf_amounts()
+    ncf_year_month = fields.Char(string='Año/Mes NCF', help="Año y mes de la transacción para el NCF.")
