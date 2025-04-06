3f5904c61e09d25f665379bdcbc558b440b84459
edy <serviciosedy@gmail.com>
2025-04-06 07:02:01 -0400

    te

--- odoo_customer_supplier_loan_app/models/loan_installment.py
+++ odoo_customer_supplier_loan_app/models/loan_installment.py
@@ -92,15 +92,7 @@ class LoanInstallment(models.Model):
 
         products_updated = 0
 
-        purchase_order_line_obj = self.env['report.sale.line'].search(
-            [('partner_id', '=', self.partner_id.id)])
-
-        for supplier_info2 in purchase_order_line_obj:
-            product2 = supplier_info2.product_id
-            reorder_qty2 = self._get_reorder_quantity(product2)
-            if reorder_qty2 > 0:
-                self._create_or_update_purchase_order_line(
-                    product2, reorder_qty2)
+       
 
         for supplier_info in supplier_infos:
             product = supplier_info.product_tmpl_id
