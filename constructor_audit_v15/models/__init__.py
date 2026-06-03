bc0b68cd5676960556397ef3764d638ea4dd29eb
root <root@d9f40ff532be>
2026-06-03 10:42:13 +0000

    deleet modaudit

--- constructor_audit_v15/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import audit_config
-from . import crm_lead_inherit
-from . import survey_question_inherit    # ← agregar esta línea
-from . import survey_survey_inherit
-from . import survey_user_input_inherit
